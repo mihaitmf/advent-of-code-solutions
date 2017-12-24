5ada1a5979a59db20c3cff0f80449cc6cf0ba136
mihaitmf <mihai.tmf@gmail.com>
2017-12-25 00:16:58 +0200

    Create framework for Day 7 part 1

---
+++ src/Day07/Day07Part1Solver.php
@@ -0,0 +1,42 @@
+<?php
+namespace AdventOfCode2017\Day06;
+
+use AdventOfCode2017\Common\SolutionExample;
+use AdventOfCode2017\Common\Solver;
+
+class Day07Part1Solver implements Solver
+{
+    /**
+     * @param string $input
+     * @return string
+     */
+    public function solve($input)
+    {
+        //TODO
+    }
+
+    /**
+     * @return SolutionExample[]
+     */
+    public function getExamples()
+    {
+        return [
+            SolutionExample::of(
+                'pbga (66)
+xhth (57)
+ebii (61)
+havc (66)
+ktlj (57)
+fwft (72) -> ktlj, cntj, xhth
+qoyq (66)
+padx (45) -> pbga, havc, qoyq
+tknk (41) -> ugml, padx, fwft
+jptl (61)
+ugml (68) -> gyxo, ebii, jptl
+gyxo (61)
+cntj (57)',
+                'tknk'
+            ),
+        ];
+    }
+}
