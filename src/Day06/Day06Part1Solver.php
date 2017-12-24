f0e7cf39e9fd8c3f23cdca64caa9b22cc26f704b
mihaitmf <mihai.tmf@gmail.com>
2017-12-24 16:15:59 +0200

    Implement framework for Day 6 part 1

---
+++ src/Day06/Day06Part1Solver.php
@@ -0,0 +1,30 @@
+<?php
+namespace AdventOfCode2017\Day06;
+
+use AdventOfCode2017\Common\SolutionExample;
+use AdventOfCode2017\Common\Solver;
+
+class Day06Part1Solver implements Solver
+{
+    /**
+     * @param string $input
+     * @return string
+     */
+    public function solve($input)
+    {
+        // TODO: Implement solve() method.
+    }
+
+    /**
+     * @return SolutionExample[]
+     */
+    public function getExamples()
+    {
+        return [
+            SolutionExample::of(
+                '0	2	7	0',
+                '5'
+            ),
+        ];
+    }
+}
