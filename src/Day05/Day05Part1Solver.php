a2af3df95fa1b3fbd30cdb4b251874c894cf2a5a
mihaitmf <mihai.tmf@gmail.com>
2017-12-25 22:00:05 +0200

    Create framework for Day 5 part 1

---
+++ src/Day05/Day05Part1Solver.php
@@ -0,0 +1,34 @@
+<?php
+namespace AdventOfCode2017\Day05;
+
+use AdventOfCode2017\Common\SolutionExample;
+use AdventOfCode2017\Common\Solver;
+
+class Day05Part1Solver implements Solver
+{
+    /**
+     * @param string $input
+     * @return string
+     */
+    public function solve($input)
+    {
+        //TODO
+    }
+
+    /**
+     * @return SolutionExample[]
+     */
+    public function getExamples()
+    {
+        return [
+            SolutionExample::of(
+                '0
+3
+0
+1
+-3',
+                '5'
+            ),
+        ];
+    }
+}
