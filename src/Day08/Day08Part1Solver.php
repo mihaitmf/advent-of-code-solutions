04ace927b4e12b38a11fdad728cfec70f3641b7a
mihaitmf <mihai.tmf@gmail.com>
2017-12-25 22:23:36 +0200

    Create framework for Day8 part 1

---
+++ src/Day08/Day08Part1Solver.php
@@ -0,0 +1,30 @@
+<?php
+namespace AdventOfCode2017\Day08;
+
+use AdventOfCode2017\Common\SolutionExample;
+use AdventOfCode2017\Common\Solver;
+
+class Day08Part1Solver implements Solver
+{
+    /**
+     * @param string $input
+     * @return string
+     */
+    public function solve($input)
+    {
+        //TODO
+    }
+
+    /**
+     * @return SolutionExample[]
+     */
+    public function getExamples()
+    {
+        return [
+            SolutionExample::of(
+                '',
+                ''
+            ),
+        ];
+    }
+}
