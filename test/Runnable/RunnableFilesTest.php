024b7b9c43e5411cb1e4130cc9e920b786340b9d
mihaitmf <mihai.tmf@gmail.com>
2018-11-30 22:40:47 +0200

    test

--- test/Runnable/RunnableFilesTest.php
+++ test/Runnable/RunnableFilesTest.php
@@ -9,12 +9,10 @@ class RunnableFilesTest extends TestCase
 {
     private static $RUNNABLE_DIR_PATH;
 
-    /**
-     * @dataProvider providerRunnableFiles
-     */
-    public function testAllRunnableFilesPrintLessThan10Characters($runnableFilename)
+    public function testAllRunnableFilesPrintLessThan10Characters()
     {
-        $runnableDirPath = self::$RUNNABLE_DIR_PATH;
+        $runnableDirPath = dirname(dirname(__DIR__)) . DIRECTORY_SEPARATOR . "runnable";
+        $runnableFilename = "02-checksum-part2.php";
         $output = shell_exec("cd {$runnableDirPath} && php {$runnableFilename}");
 
         $errorMessage = "Output was bigger than 10 characters which most likely means it was an error."
@@ -23,23 +21,37 @@ class RunnableFilesTest extends TestCase
         $this->assertLessThan(10, strlen($output), $errorMessage);
     }
 
-    public function providerRunnableFiles()
-    {
-        $dataProviderArray = [];
-
-        self::$RUNNABLE_DIR_PATH = dirname(dirname(__DIR__)) . DIRECTORY_SEPARATOR . "runnable";
-        $knownForFailingList = [
-            "03-spiral-matrix-part2.php",
-        ];
-
-        foreach (new DirectoryIterator(self::$RUNNABLE_DIR_PATH) as $file) {
-            $filename = $file->getFilename();
-
-            if ($file->isFile() && $file->getExtension() === "php" && !in_array($filename, $knownForFailingList, true)) {
-                $dataProviderArray["running {$filename}"] = [$filename];
-            }
-        }
-
-        return $dataProviderArray;
-    }
+//    /**
+//     * @dataProvider providerRunnableFiles
+//     */
+//    public function testAllRunnableFilesPrintLessThan10Characters($runnableFilename)
+//    {
+//        $runnableDirPath = self::$RUNNABLE_DIR_PATH;
+//        $output = shell_exec("cd {$runnableDirPath} && php {$runnableFilename}");
+//
+//        $errorMessage = "Output was bigger than 10 characters which most likely means it was an error."
+//            . "\nFile name: {$runnableFilename}" . "\nOutput: {$output}";
+//
+//        $this->assertLessThan(10, strlen($output), $errorMessage);
+//    }
+//
+//    public function providerRunnableFiles()
+//    {
+//        $dataProviderArray = [];
+//
+//        self::$RUNNABLE_DIR_PATH = dirname(dirname(__DIR__)) . DIRECTORY_SEPARATOR . "runnable";
+//        $knownForFailingList = [
+//            "03-spiral-matrix-part2.php",
+//        ];
+//
+//        foreach (new DirectoryIterator(self::$RUNNABLE_DIR_PATH) as $file) {
+//            $filename = $file->getFilename();
+//
+//            if ($file->isFile() && $file->getExtension() === "php" && !in_array($filename, $knownForFailingList, true)) {
+//                $dataProviderArray["running {$filename}"] = [$filename];
+//            }
+//        }
+//
+//        return $dataProviderArray;
+//    }
 }
