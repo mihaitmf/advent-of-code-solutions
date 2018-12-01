f7deef5843ca59242dd2cbbf7cce3fe7e69283e0
mihaitmf <mihai.tmf@gmail.com>
2018-12-01 05:11:19 +0200

    Create a new application entry point file common for all the solutions

---
+++ run.php
@@ -0,0 +1,19 @@
+<?php
+
+use AdventOfCode2017\Common\DaysSolversMapper;
+use AdventOfCode2017\Common\SolutionRunner;
+
+require_once 'bootstrap.php';
+
+if ($argc !== 3) {
+    print("The solution runner requires two integer arguments: the day of the problem and the part. Example run solution for day 9, part 2: php run.php 9 2");
+    exit(1);
+}
+
+try {
+    $output = (new SolutionRunner(new DaysSolversMapper()))->run($argv[1], $argv[2]);
+} catch (\Exception $exception) {
+    $output = $exception->getMessage();
+}
+
+print($output);
