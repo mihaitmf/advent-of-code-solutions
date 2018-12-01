e6c53076ef42c576630833e668fe7ada8d9a810e
mihaitmf <mihai.tmf@gmail.com>
2018-12-01 06:31:29 +0200

    Add integration test that checks the expected output for each solution
    It helps with regression testing when refactoring

---
+++ test/Common/SolutionRunnerIntegrationTest.php
@@ -0,0 +1,37 @@
+<?php
+namespace AdventOfCode2017\Tests\Common;
+
+use AdventOfCode2017\Common\DaysSolversMapper;
+use AdventOfCode2017\Common\SolutionRunner;
+use PHPUnit\Framework\TestCase;
+
+class SolutionRunnerIntegrationTest extends TestCase
+{
+    /** @var SolutionRunner */
+    private $solutionRunner;
+
+    protected function setUp()
+    {
+        $this->solutionRunner = new SolutionRunner(new DaysSolversMapper());
+    }
+
+    /**
+     * @dataProvider providerSolutions
+     */
+    public function testRunSolutionsExpectedOutput($day, $part, $expectedResult)
+    {
+        $actualResult = $this->solutionRunner->run($day, $part);
+
+        $this->assertSame($expectedResult, $actualResult, "The solution runner returned a different result");
+    }
+
+    public function providerSolutions()
+    {
+        return [
+            "Day 01 Part 1" => [1, 1, "1251"],
+            "Day 01 Part 2" => [1, 2, "1244"],
+            "Day 02 Part 1" => [2, 1, "43074"],
+            "Day 02 Part 2" => [2, 2, "280"],
+        ];
+    }
+}
