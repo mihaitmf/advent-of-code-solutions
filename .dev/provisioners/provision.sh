854f4e3787827986d7b03c119279b6ab2c8cb158
mihaitmf <mihai.tmf@gmail.com>
2018-12-02 23:15:36 +0200

    Setup symlink to run the php from the docker container

--- .dev/provisioners/provision.sh
+++ .dev/provisioners/provision.sh
@@ -3,3 +3,6 @@
 # set working directory after vagrant ssh
 WORK_DIR=/var/advent-of-code-solutions
 echo "cd $WORK_DIR" >> /home/vagrant/.bashrc
+
+# this symbolic link makes possible to use "/usr/bin/php" as interpreter path in IDE settings
+ln --force --symbolic ${WORK_DIR}/run-php-container.sh /usr/bin/php
