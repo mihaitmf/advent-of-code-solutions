a3a7d83cce7871beb27ceb32f66a70ad1be8ffa6
mihaitmf <mihai.tmf@gmail.com>
2018-12-02 23:14:01 +0200

    Use absolute path for working directory in run-php-container.sh

--- run-php-container.sh
+++ run-php-container.sh
@@ -3,12 +3,12 @@
 COMMAND="$@"
 IMAGE_NAME="advent-of-code/php:latest"
 
-HOST_WORK_DIR=$(realpath $(dirname "$0"))
-CONTAINER_WORK_DIR=/var/advent-of-code-solutions
+HOST_WORK_DIR=/var/advent-of-code-solutions
+CONTAINER_WORK_DIR=/advent-of-code-solutions
 HOST_CONFIG_FILE_PATH=${HOST_WORK_DIR}/dev/config/custom-php.ini
 CONTAINER_CONFIG_FILE_PATH=/usr/local/etc/php/conf.d/custom-php.ini
 
-XDEBUG_STATUS=1
+XDEBUG_STATUS=1 # enable XDebug by default
 XDEBUG_HOST="192.168.30.11"
 
 docker run --rm \
