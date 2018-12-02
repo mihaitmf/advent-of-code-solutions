df8a8b9d6b9cdec44f473cea35c730e3e11196e2
mihaitmf <mihai.tmf@gmail.com>
2018-12-02 22:40:31 +0200

    Add shell script to run the php docker container

---
+++ run-php-container.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+COMMAND="$@"
+IMAGE_NAME="advent-of-code/php:latest"
+
+HOST_WORK_DIR=$(realpath $(dirname "$0"))
+CONTAINER_WORK_DIR=/var/advent-of-code-solutions
+HOST_CONFIG_FILE_PATH=${HOST_WORK_DIR}/dev/config/custom-php.ini
+CONTAINER_CONFIG_FILE_PATH=/usr/local/etc/php/conf.d/custom-php.ini
+
+XDEBUG_STATUS=1
+XDEBUG_HOST="192.168.30.11"
+
+docker run --rm \
+    --workdir ${CONTAINER_WORK_DIR} \
+    --volume ${HOST_WORK_DIR}:${CONTAINER_WORK_DIR} \
+    --volume ${HOST_CONFIG_FILE_PATH}:${CONTAINER_CONFIG_FILE_PATH} \
+    --env "XDEBUG_STATUS=$XDEBUG_STATUS" \
+    --env "XDEBUG_HOST=$XDEBUG_HOST" \
+    ${IMAGE_NAME} "$COMMAND"
+
+## Use this to debug inside the container
+#docker run -it --entrypoint bash \
+#    --workdir ${CONTAINER_WORK_DIR} \
+#    --volume ${HOST_WORK_DIR}:${CONTAINER_WORK_DIR} \
+#    --volume ${HOST_CONFIG_FILE_PATH}:${CONTAINER_CONFIG_FILE_PATH} \
+#    --env "XDEBUG_STATUS=$XDEBUG_STATUS" \
+#    --env "XDEBUG_HOST=$XDEBUG_HOST" \
+#    ${IMAGE_NAME}
