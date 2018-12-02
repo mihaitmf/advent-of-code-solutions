fe1c65833236b908e502a6c52d2ae004432550e3
mihaitmf <mihai.tmf@gmail.com>
2018-12-02 13:36:36 +0200

    Update fast-solution runner and add code inside a function to be able to quickly return a value

--- fast-solution/run.php
+++ fast-solution/run.php
@@ -1,40 +1,41 @@
 <?php
 
 $input = trim(file_get_contents("input.txt"));
-
-$result = 0;
-
-// single string input
-$input = str_split($input);
-foreach ($input as $i => $currentChar) {
-    $currentChar;
-}
-
-// single row input
-$items = parseItems($input);
-foreach ($items as $i => $item) {
-    $item;
-}
-
-// single column input
-$items = parseRows($input);
-foreach ($items as $i => $item) {
-    $item;
-}
-
-// matrix input
-$matrix = parseMatrix($input);
-$rows = parseRows($input);
-foreach ($rows as $rowIndex => $row) {
-    $items = parseItems($row);
-
-    foreach ($items as $itemIndex => $item) {
-        $item;
-    }
-}
-
+$result = "";
+$result = solve($input);
 print($result);
 
+function solve($input)
+{
+    // single column input (multiple rows with a single value)
+    $items = parseRows($input);
+    foreach ($items as $i => $item) {
+        $item;
+    }
+
+    // single string input
+    $input = str_split($input);
+    foreach ($input as $i => $currentChar) {
+        $currentChar;
+    }
+
+    // single row input
+    $items = parseItems($input);
+    foreach ($items as $i => $item) {
+        $item;
+    }
+
+    // matrix input
+    $matrix = parseMatrix($input);
+    $rows = parseRows($input);
+    foreach ($rows as $rowIndex => $row) {
+        $items = parseItems($row);
+
+        foreach ($items as $itemIndex => $item) {
+            $item;
+        }
+    }
+}
 
 function parseRows($input) {
     return explode("\n", $input);
