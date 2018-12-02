2d9e1ead506c8aadcd5def952ec0417b30f9a52d
mihaitmf <mihai.tmf@gmail.com>
2018-12-02 23:31:58 +0200

    Build docker image in vagrant provisioner

--- .dev/provisioners/provision.sh
+++ .dev/provisioners/provision.sh
@@ -4,5 +4,8 @@
 WORK_DIR=/var/advent-of-code-solutions
 echo "cd $WORK_DIR" >> /home/vagrant/.bashrc
 
-# this symbolic link makes possible to use "/usr/bin/php" as interpreter path in IDE settings
+# build docker image
+${WORK_DIR}/build.sh
+
+# create symbolic link to use "/usr/bin/php" as interpreter path in IDE settings
 ln --force --symbolic ${WORK_DIR}/run-php-container.sh /usr/bin/php
