342bc1bd4bf22582eb8b806bf623423afc47ce54
mihaitmf <mihai.tmf@gmail.com>
2018-12-02 02:08:25 +0200

    Add config.ini file to set default arguments for the runner when CLI arguments are not given

--- run.php
+++ run.php
@@ -5,13 +5,28 @@ use AdventOfCode\Common\SolutionRunner;
 
 require_once "bootstrap.php";
 
-if ($argc !== 4) {
+$configFile = "config.ini";
+
+// all 3 command line arguments
+if ($argc === 4) {
+    $year = $argv[1];
+    $day = $argv[2];
+    $part = $argv[3];
+
+// no command line arguments, read defaults from config file
+} elseif ($argc === 1 && is_file($configFile)) {
+    $config = parse_ini_file($configFile, true);
+    $year = $config["runner-default"]["year"];
+    $day = $config["runner-default"]["day"];
+    $part = $config["runner-default"]["part"];
+
+} else {
     print("The solution runner requires three integer arguments: the year of the event, the day and the part of the problem. Example run solution for the 2017 event, day 9, part 2: php run.php 2017 9 2");
     exit(1);
 }
 
 try {
-    $output = (new SolutionRunner(new DaysSolversMapper()))->run($argv[1], $argv[2], $argv[3]);
+    $output = (new SolutionRunner(new DaysSolversMapper()))->run($year, $day, $part);
 } catch (\Exception $exception) {
     $output = $exception->getMessage();
 }
