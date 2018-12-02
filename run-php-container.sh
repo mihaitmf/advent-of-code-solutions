40774c10556974047e652b0ee4efdc0aefd0f0a2
mihaitmf <mihai.tmf@gmail.com>
2018-12-02 23:56:09 +0200

    Fix container volume name to be the same as the host working directory

--- run-php-container.sh
+++ run-php-container.sh
@@ -3,8 +3,8 @@
 COMMAND="$@"
 IMAGE_NAME="advent-of-code/php:latest"
 
-HOST_WORK_DIR=$(realpath $(dirname "$0"))
-CONTAINER_WORK_DIR=/advent-of-code-solutions
+HOST_WORK_DIR=$(realpath $(dirname $(readlink -f "$0")))
+CONTAINER_WORK_DIR=${HOST_WORK_DIR}
 HOST_CONFIG_FILE_PATH=${HOST_WORK_DIR}/dev/config/custom-php.ini
 CONTAINER_CONFIG_FILE_PATH=/usr/local/etc/php/conf.d/custom-php.ini
 
