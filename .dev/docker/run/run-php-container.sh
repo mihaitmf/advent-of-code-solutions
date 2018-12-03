3c80f2d3cb8feaf8f8c7e66473cf92a005450557
mihaitmf <mihai.tmf@gmail.com>
2018-12-03 03:08:09 +0200

    Separate build and run directories

--- .dev/docker/run/run-php-container.sh
+++ .dev/docker/run/run-php-container.sh
@@ -7,14 +7,14 @@ HOST_WORK_DIR=$(realpath $(dirname $(readlink -f "$0"))/../..)
 CONTAINER_WORK_DIR=${HOST_WORK_DIR}
 
 # needed to sync php config file
-HOST_CONFIG_FILE_PATH=${HOST_WORK_DIR}/.dev/docker/config/custom-php.ini
+HOST_CONFIG_FILE_PATH=${HOST_WORK_DIR}/.dev/docker/run/config/custom-php.ini
 CONTAINER_CONFIG_FILE_PATH=/usr/local/etc/php/conf.d/custom-php.ini
 
 # needed for using Remote PHP Interpreter in IDE
 PHPSTORM_HELPERS_DIR=/home/vagrant/.phpstorm_helpers
 
 # needed for using XDebug from the IDE
-SETTINGS_ENV_FILE_PATH=${HOST_WORK_DIR}/.dev/docker/config/xdebug_settings.env
+SETTINGS_ENV_FILE_PATH=${HOST_WORK_DIR}/.dev/docker/run/config/xdebug_settings.env
 
 docker run --rm \
     --workdir ${CONTAINER_WORK_DIR} \
--- .dev/provisioners/provision.sh
+++ .dev/provisioners/provision.sh
@@ -6,10 +6,10 @@ WORK_DIR=/var/advent-of-code-solutions
 echo "cd $WORK_DIR" >> /home/vagrant/.bashrc
 
 # build docker image
-${WORK_DIR}/.dev/docker/build.sh
+${WORK_DIR}/.dev/docker/build/build.sh
 
 # create symbolic link to use "/usr/bin/php" as interpreter path in IDE settings
-ln --force --symbolic ${WORK_DIR}/.dev/docker/run-php-container.sh /usr/bin/php
+ln --force --symbolic ${WORK_DIR}/.dev/docker/run/run-php-container.sh /usr/bin/php
 
 # needed for using Remote PHP Interpreter in IDE
 mkdir /home/vagrant/.phpstorm_helpers
