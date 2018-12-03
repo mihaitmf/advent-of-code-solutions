061e7b9f3b9552a030ba855e3bb4b398afafea13
mihaitmf <mihai.tmf@gmail.com>
2018-12-03 05:17:13 +0200

    Run composer install at vagrant provisioning

--- .dev/provisioners/provision.sh
+++ .dev/provisioners/provision.sh
@@ -5,12 +5,15 @@ WORK_DIR=/var/advent-of-code-solutions
 # set working directory after vagrant ssh
 echo "cd $WORK_DIR" >> /home/vagrant/.bashrc
 
-# build docker image
-${WORK_DIR}/.dev/docker/build/build-all.sh
-
 # create symbolic link to use "/usr/bin/php" as interpreter path in IDE settings
 ln --force --symbolic ${WORK_DIR}/.dev/docker/run/run-php-container.sh /usr/bin/php
 
 # needed for using Remote PHP Interpreter in IDE
 mkdir /home/vagrant/.phpstorm_helpers
 chown vagrant:vagrant /home/vagrant/.phpstorm_helpers
+
+# build docker images
+${WORK_DIR}/.dev/docker/build/build-all.sh
+
+# run composer install
+${WORK_DIR}/.dev/docker/run/run-composer-container.sh install
