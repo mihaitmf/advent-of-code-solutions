7ff1d61806255f3fdb990dd94908304febef7adb
mihaitmf <mihai.tmf@gmail.com>
2018-12-03 02:52:54 +0200

    Fix comment

--- .dev/provisioners/provision.sh
+++ .dev/provisioners/provision.sh
@@ -11,6 +11,6 @@ ${WORK_DIR}/.dev/docker/build.sh
 # create symbolic link to use "/usr/bin/php" as interpreter path in IDE settings
 ln --force --symbolic ${WORK_DIR}/.dev/docker/run-php-container.sh /usr/bin/php
 
-# `
+# needed for using Remote PHP Interpreter in IDE
 mkdir /home/vagrant/.phpstorm_helpers
 chown vagrant:vagrant /home/vagrant/.phpstorm_helpers
