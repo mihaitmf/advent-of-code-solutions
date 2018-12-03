4d4b9a1f80cfe24bc86f1168f9413d374b267ae7
mihaitmf <mihai.tmf@gmail.com>
2018-12-03 02:46:29 +0200

    Move shell scripts and all files used for building the development environment into the .dev directory

--- .dev/docker/run-php-container.sh
+++ .dev/docker/run-php-container.sh
@@ -2,9 +2,9 @@
 
 IMAGE_NAME="advent-of-code/php:latest"
 
-HOST_WORK_DIR=$(realpath $(dirname $(readlink -f "$0")))
+HOST_WORK_DIR=$(realpath $(dirname $(readlink -f "$0"))/../..)
 CONTAINER_WORK_DIR=${HOST_WORK_DIR}
-HOST_CONFIG_FILE_PATH=${HOST_WORK_DIR}/dev/config/custom-php.ini
+HOST_CONFIG_FILE_PATH=${HOST_WORK_DIR}/.dev/docker/config/custom-php.ini
 CONTAINER_CONFIG_FILE_PATH=/usr/local/etc/php/conf.d/custom-php.ini
 
 XDEBUG_STATUS=1 # enable XDebug by default
@@ -16,7 +16,6 @@ docker run --rm \
     --volume ${HOST_CONFIG_FILE_PATH}:${CONTAINER_CONFIG_FILE_PATH} \
     --env "XDEBUG_STATUS=$XDEBUG_STATUS" \
     --env "XDEBUG_HOST=$XDEBUG_HOST" \
-    --env-file "${HOST_WORK_DIR}/dev/config/settings.env" \
     ${IMAGE_NAME} "$@"
 
 ## Use this to debug inside the container
--- .dev/provisioners/provision.sh
+++ .dev/provisioners/provision.sh
@@ -6,7 +6,7 @@ WORK_DIR=/var/advent-of-code-solutions
 echo "cd $WORK_DIR" >> /home/vagrant/.bashrc
 
 # build docker image
-${WORK_DIR}/build.sh
+${WORK_DIR}/.dev/docker/build.sh
 
 # create symbolic link to use "/usr/bin/php" as interpreter path in IDE settings
-ln --force --symbolic ${WORK_DIR}/run-php-container.sh /usr/bin/php
+ln --force --symbolic ${WORK_DIR}/.dev/docker/run-php-container.sh /usr/bin/php
