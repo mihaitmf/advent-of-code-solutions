1142f3a7a93693931222de86ae8f48de392dc493
mihaitmf <mihai.tmf@gmail.com>
2018-12-03 05:12:18 +0200

    Create build-all.sh script to be called by the vagrant provisioner

--- .dev/provisioners/provision.sh
+++ .dev/provisioners/provision.sh
@@ -6,7 +6,7 @@ WORK_DIR=/var/advent-of-code-solutions
 echo "cd $WORK_DIR" >> /home/vagrant/.bashrc
 
 # build docker image
-${WORK_DIR}/.dev/docker/build/build.sh
+${WORK_DIR}/.dev/docker/build/build-all.sh
 
 # create symbolic link to use "/usr/bin/php" as interpreter path in IDE settings
 ln --force --symbolic ${WORK_DIR}/.dev/docker/run/run-php-container.sh /usr/bin/php
