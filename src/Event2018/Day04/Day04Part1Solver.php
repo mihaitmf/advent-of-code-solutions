876ac19e181b34ebd16b208d1ff087a264132ce4
mihaitmf <mihai.tmf@gmail.com>
2018-12-04 17:54:02 +0200

    Start implementation for 2018, day 4

---
+++ src/Event2018/Day04/Day04Part1Solver.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace AdventOfCode\Event2018\Day04;
+
+use AdventOfCode\Common\InputParser;
+use AdventOfCode\Common\Solver;
+use RuntimeException;
+
+/**
+ * http://adventofcode.com/2018/day/4
+ *
+
+ */
+class Day04Part1Solver implements Solver
+{
+    /**
+     * @Inject
+     * @var InputParser
+     */
+    private $inputParser;
+
+    /**
+     * @param string $input
+     *
+     * @return string
+     */
+    public function solve($input)
+    {
+        $items = $this->inputParser->parseRows($input);
+
+        $itemsByDate = [];
+
+        foreach ($items as $item) {
+            $matches = [];
+            $matchResult = preg_match("/^\[\d{4}-(\d+-\d+ \d\d:\d\d)\] (.*)/", $item, $matches);
+            if ($matchResult !== 1) {
+                throw new RuntimeException("Could not parse input");
+            }
+
+            $itemsByDate[$matches[1]] = $matches[2];
+        }
+
+        $sortedDates = array_keys($itemsByDate);
+        sort($sortedDates);
+        var_dump($sortedDates);
+    }
+}
