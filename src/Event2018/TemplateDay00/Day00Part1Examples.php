a9ec85ff0244bda4a9af9a199535a141707df14e
mihaitmf <mihai.tmf@gmail.com>
2018-12-05 02:52:01 +0200

    Add template for the solver and example classes needed for one Day

---
+++ src/Event2018/TemplateDay00/Day00Part1Examples.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace AdventOfCode\Event2018\Day00;
+
+use AdventOfCode\Common\ExamplesProvider;
+use AdventOfCode\Common\SolutionExample;
+
+class Day00Part1Examples implements ExamplesProvider
+{
+    /**
+     * @return SolutionExample[]
+     */
+    public function getExamples()
+    {
+        return [
+            SolutionExample::of(
+                "",
+                ""
+            ),
+        ];
+    }
+}
